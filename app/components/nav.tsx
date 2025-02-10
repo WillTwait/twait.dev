ca54c34f9828efa4e8291bd6e75fdbd0d8a37abb
Will Twait <will.twait@gmail.com>
2025-02-10 10:48:41 -0500

    fix: include icons in desktop nav sidebar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -16,59 +16,62 @@ import { usePathname } from "next/navigation";
 const navItems = {
   "/": {
     name: "Home",
-    icon: <House />,
+    icon: <House className="md:size-5" />,
   },
   "/thoughts": {
     name: "Thoughts",
-    icon: <NotebookPen />,
+    icon: <NotebookPen className="md:size-5" />,
   },
   "/work": {
     name: "Work",
-    icon: <Briefcase />,
+    icon: <Briefcase className="md:size-5" />,
   },
   "/projects": {
     name: "Projects",
-    icon: <Hammer />,
+    icon: <Hammer className="md:size-5" />,
   },
   "/books": {
     name: "Books",
-    icon: <LibraryBig />,
+    icon: <LibraryBig className="md:size-5" />,
   },
   "/blogroll": {
     name: "Blogroll",
-    icon: <Newspaper />,
+    icon: <Newspaper className="md:size-5" />,
   },
   "/uses": {
     name: "Uses",
-    icon: <Laptop />,
+    icon: <Laptop className="md:size-5" />,
   },
   "/about": {
     name: "About",
-    icon: <User />,
+    icon: <User className="md:size-5" />,
   },
 };
 
 export function Navbar() {
   const pathname = usePathname();
+  const basePathname = `/${pathname?.split("/")[1] || ""}`;
 
   return (
     <aside className="tracking-tight bg-white">
       <nav className="flex flex-col space-y-0.5 md:space-y-1">
         {Object.entries(navItems).map(([path, { name, icon }]) => {
-          const isActive = pathname === path;
+          const isActive = basePathname === path;
           return (
             <Link
               key={path}
               href={path}
-              className={`group border-2 border-transparent w-12 md:w-26 text-center rounded-sm hover:border-black flex items-center relative py-0.5 md:py-1 px-1.5 md:px-2 text-xs md:text-base ${
+              className={`group border-2 border-transparent w-12 md:w-32 text-center rounded-sm hover:border-black flex items-center relative py-0.5 md:py-1 px-1.5 md:px-2 text-xs md:text-base ${
                 isActive ? "font-bold" : "font-normal"
               }`}
             >
               {isActive && (
                 <span className="size-1 md:size-1.5 mr-1 md:mr-1.5 rounded-full bg-black" />
               )}
-              <span className="md:hidden">{icon}</span>
-              <span className="hidden md:inline">{name}</span>
+              <div className="flex flex-row items-center gap-1">
+                <span>{icon}</span>
+                <span className="hidden md:inline">{name}</span>
+              </div>
             </Link>
           );
         })}
