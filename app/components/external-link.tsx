3c0ab2037cf1a7da20b6cc1a3a29b5be73bcae65
Will Twait <will.twait@gmail.com>
2025-02-19 12:05:33 -0500

    fix inline flex

--- app/components/external-link.tsx
+++ app/components/external-link.tsx
@@ -20,13 +20,15 @@ export default function ExternalLink({ href, children }: ExternalLinkProps) {
   const domain = new URL(href).hostname.replace("www.", "");
 
   return (
-    <a href={href} className="inline-flex items-center gap-1">
+    <a href={href} className="inline items-center">
       {children}
-      <img
-        src={`https://www.google.com/s2/favicons?domain=${domain}&sz=16`}
-        alt={`${domain} favicon`}
-        className="size-3 rounded-sm inline-block"
-      />
+      <span className="pl-1 inline-flex justify-center">
+        <img
+          src={`https://www.google.com/s2/favicons?domain=${domain}&sz=16`}
+          alt={`${domain} favicon`}
+          className="size-3 rounded-sm inline"
+        />
+      </span>
     </a>
   );
 }
