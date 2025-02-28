0ed4276f15bb92b8f0d4a7855ecd6feb67bacb05
Will Twait <will.twait@gmail.com>
2025-02-28 14:35:43 -0500

    fix: try to fix rss

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -2,7 +2,7 @@ import { baseUrl } from "app/sitemap";
 import { getBlogPosts } from "app/thoughts/utils";
 
 export async function GET() {
-  const allBlogs = await getBlogPosts();
+  const allBlogs = getBlogPosts();
 
   const itemsXml = allBlogs
     .sort((a, b) => {
@@ -15,28 +15,33 @@ export async function GET() {
       (post) =>
         `<item>
           <title>${post.metadata.title}</title>
-          <link>${baseUrl}/blog/${post.slug}</link>
+          <link>${baseUrl}/thoughts/${post.slug}</link>
           <description>${post.metadata.summary || ""}</description>
           <pubDate>${new Date(
             post.metadata.publishedAt
           ).toUTCString()}</pubDate>
+          <guid>${baseUrl}/thoughts/${post.slug}</guid>
         </item>`
     )
     .join("\n");
 
   const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
-  <rss version="2.0">
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
-        <title>My Portfolio</title>
+        <title>Will Twait's Website</title>
         <link>${baseUrl}</link>
-        <description>This is my portfolio RSS feed</description>
+        <description>Thoughts and ideas from Will</description>
+        <language>en-us</language>
+        <atom:link href="${baseUrl}/rss" rel="self" type="application/rss+xml" />
         ${itemsXml}
     </channel>
   </rss>`;
 
   return new Response(rssFeed, {
     headers: {
-      "Content-Type": "text/xml",
+      "Content-Type": "application/xml",
+      "Cache-Control":
+        "max-age=3600, s-maxage=3600, stale-while-revalidate=86400",
     },
   });
 }
